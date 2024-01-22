d08959cbb239921e15ce2b3cf9110464dc1f0659
Jane <hezha.aban@gmail.com>
2024-01-22 17:55:19 +0100

    Add project files.

---
+++ StudentEnrollment/Entities/StudentEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentEnrollment.Entities
+{
+    public class StudentEntity
+    {
+        //Primärnyckel (Primary Key)
+        public int StudentId { get; set; }
+
+        //övriga egenskaper
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+
+
+
+
+    }
+}
---
+++ StudentEnrollment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentEnrollment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
