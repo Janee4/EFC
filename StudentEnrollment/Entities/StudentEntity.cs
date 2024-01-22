c40e18a512181b12230375fdf7f4bb33163c08af
Jane <hezha.aban@gmail.com>
2024-01-22 18:00:05 +0100

    Gjort en entitet (student)

--- StudentEnrollment/Entities/StudentEntity.cs
+++ StudentEnrollment/Entities/StudentEntity.cs
@@ -2,14 +2,14 @@
 {
     public class StudentEntity
     {
-        //Primärnyckel (Primary Key)
+        //Primärnyckel (Primary Key) som kommer behövas då varje student kommer att ha en unik identifikation
         public int StudentId { get; set; }
 
         //övriga egenskaper
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
         public int Grade { get; set; }
-
 
 
 
