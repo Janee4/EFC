d80384c462e9f619f02fddc5c195f66d36e409d2
Jane <hezha.aban@gmail.com>
2024-01-29 13:15:15 +0100

    Fixat datacontext inuti dependency injection i program

--- StudentEnrollment/Program.cs
+++ StudentEnrollment/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace StudentEnrollment
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentEnrollment.Contexts;
+
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureServices(services =>
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+    //Registera datacontext inuti dependency injection så att vi kan använda denna i hela vår applikation utan att behöva specifiera upp context delen i olika filer repeterande gånger
+    services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CSharp-Projects\EFC\StudentEnrollment\Data\local_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True")
+
+    
+});
+
+builder.Build();
