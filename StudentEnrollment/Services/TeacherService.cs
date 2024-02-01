b16f76efdbd472019ecfc16e70e80c2681b44cc9
Jane <hezha.aban@gmail.com>
2024-02-01 16:43:34 +0100

    fixat teacher entity lite servicen

---
+++ StudentEnrollment/Services/TeacherService.cs
@@ -0,0 +1,64 @@
+﻿using StudentEnrollment.Entities;
+using StudentEnrollment.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Services
+{
+    internal class TeacherService
+    {
+        private readonly TeacherRepository _teacherRepository;
+
+        public TeacherService(TeacherRepository teacherRepository)
+        {
+            _teacherRepository = teacherRepository;
+        }
+
+
+        public TeacherEntity CreateTeacher(string firstName, string lastName, string subject)
+        {
+            var teacherEntity = _teacherRepository.Get(x => x.FirstName == firstName && x.LastName == lastName && x.Subject == subject);
+
+            teacherEntity ??= _teacherRepository.Create(new TeacherEntity { FirstName = firstName, LastName = lastName, Subject = subject  });
+
+            return teacherEntity;
+        }
+
+        public TeacherEntity GetAssignmentByAssignmentName(string teacherName)
+        {
+            var assignmentEntity = _teacherRepository.Get(x => x.TeacherName == teacherName);
+            return assignmentEntity;
+
+        }
+
+        public TeacherEntity GetAssignmentById(int id)
+        {
+            var teacherEntity = _teacherRepository.Get(x => x.TeacherId == id);
+            return teacherEntity;
+        }
+
+        public IEnumerable<TeacherEntity> GetAssignments()
+        {
+            var assignments = _teacherRepository.GetAll();
+            return assignments;
+
+        }
+
+        public TeacherEntity UpdateAssignment(TeacherEntity assignmentEntity)
+        {
+            var updatedAssignmentEntity = _teacherRepository.Update(x => x.TeacherId == TeacherEntity.TeacherId, TeacherEntity);
+            return updatedAssignmentEntity;
+
+        }
+
+        public void DeleteAssignment(int AssignmentId)
+        {
+            _teacherRepository.Delete(x => x.AssignmentId == AssignmentId);
+
+        }
+
+    }
+}
