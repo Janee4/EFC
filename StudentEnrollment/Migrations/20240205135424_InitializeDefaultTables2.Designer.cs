5bddac4f279f8b2d0f4ad73946a6d3ff6cc2669e
Jane <hezha.aban@gmail.com>
2024-02-05 15:31:14 +0100

    sparat därifrån jag frågat om jag är klar

--- StudentEnrollment/Migrations/20240205135424_InitializeDefaultTables2.Designer.cs
+++ StudentEnrollment/Migrations/20240205135424_InitializeDefaultTables2.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentEnrollment.Contexts;
 
@@ -10,9 +11,11 @@ using StudentEnrollment.Contexts;
 namespace StudentEnrollment.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240205135424_InitializeDefaultTables2")]
+    partial class InitializeDefaultTables2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentEnrollment/Migrations/20240205135424_InitializeDefaultTables2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentEnrollment.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDefaultTables2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CourseID",
+                table: "Courses",
+                newName: "CourseId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssignmentName",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignmentName",
+                table: "Assignments");
+
+            migrationBuilder.RenameColumn(
+                name: "CourseId",
+                table: "Courses",
+                newName: "CourseID");
+        }
+    }
+}
